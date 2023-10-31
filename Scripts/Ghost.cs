bc8fbc6c89b3ed3ea26e44fde786b9583fc1486c
12FeetUp <12feetup@gmail.com>
2023-10-31 11:51:00 -0700

    I Ain't Afraid of No Ghost
    Changed ghost image to an AnimatedSprite2D and added an idle animation

--- Scripts/Ghost.cs
+++ Scripts/Ghost.cs
@@ -3,9 +3,17 @@ using System;
 
 public partial class Ghost : Area2D
 {
+	//This extra code isn't needed for now, because there's only one animation.
+	//It will come in handy if we add a second animation and want to switch from this script.
+	//private AnimatedSprite2D Anim;
+		
 	public override void _Ready()
 	{
 		BodyEntered += PlayerCollision;
+		//Anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		//Anim.Play();
+		
+		
 	}
 
 	void PlayerCollision(Node2D player)
