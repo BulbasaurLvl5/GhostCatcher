d53ea3c6f75f4f61d1dbbb0e71e62a87e41af8b2
SK <sknauer@gmx.net>
2023-11-04 19:01:46 +0100

    added test level as level_2

--- Scripts/LevelLoader.cs
+++ Scripts/LevelLoader.cs
@@ -12,11 +12,21 @@ public static class LevelLoader
 	static PackedScene packedPlatforms = ResourceLoader.Load<PackedScene>("res://Scenes/platforms.tscn");
 	static PackedScene packedGhost = ResourceLoader.Load<PackedScene>("res://Scenes/ghost.tscn");
 
+	//levels
+	static PackedScene packedTestLevel = ResourceLoader.Load<PackedScene>("res://Scenes/test_level.tscn");
 
 	public static Action<Main>[] LoadLevel = {
 		LoadLevel_0,
-		LoadLevel_1
+		LoadLevel_1,
+		LoadLevel_2
 		};
+
+	public static async void PlayerDisableDelay(Main _main, int milisecdelay)
+	{
+		//delay the activation just a little bit, to allow the camera to lock
+		await Task.Delay(milisecdelay);
+		_main.player.ProcessMode = Node.ProcessModeEnum.Disabled;
+	}
 
     static void LoadLevel_0(Main _main)
     {
@@ -91,12 +101,6 @@ public static class LevelLoader
 		PlayerDisableDelay(_main, 1);
     }
 
-	public static async void PlayerDisableDelay(Main _main, int milisecdelay)
-	{
-		await Task.Delay(milisecdelay);
-		_main.player.ProcessMode = Node.ProcessModeEnum.Disabled;
-	}
-
 	static void LoadLevel_1(Main _main)
     {
 		_main.StartLevel(1);
@@ -161,4 +165,27 @@ public static class LevelLoader
 
 		PlayerDisableDelay(_main, 1);
     }
+
+	static void LoadLevel_2(Main _main)
+    {
+		_main.StartLevel(2);
+
+		_main.player = packedPlayer.Instantiate(_main.World, new Vector2(3730,810), 0);
+		// _main.player.ProcessMode = Node.ProcessModeEnum.Disabled;
+
+		TimeLabel _timeLabel = packedTimeLabel.Instantiate<TimeLabel>();
+		// _timeLabel.GlobalPosition = new Vector2(1200,50);
+		_main.UI.AddChild(_timeLabel);
+
+		MainLabel _center_label = packedCenterLabel.Instantiate<MainLabel>();
+		_main.UI.AddChild(_center_label);
+
+		Node _testlevel = packedTestLevel.Instantiate(_main.World) as Node;
+
+		Ghost _g = packedGhost.Instantiate(_main.World, new Vector2I(3600, -800), 0) as Ghost;
+		_g.TreeExited += _main.GhostTreeExit;
+		_main.GhostCount += 1;
+
+		PlayerDisableDelay(_main, 1);
+	}
 }
